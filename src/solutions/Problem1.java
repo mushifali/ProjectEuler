4699546abda246094c52fc150bf3e4d7093e4a17
Mushif Ali Nawaz <mushif07@gmail.com>
2019-02-20 09:05:16 +0500

    Problem 1 Solved

--- src/solutions/Problem1.java
+++ src/solutions/Problem1.java
@@ -1,13 +1,18 @@
 package solutions;
 
+import java.util.stream.IntStream;
+
 /**
  * @author Mushif Ali Nawaz
  * Date    February 20, 2019
- *
+ * <p>
  * Problem 1: Multiples of 3 and 5
  */
 public class Problem1 {
     public static void main(String[] args) {
+        int result = IntStream.range(3, 1000).parallel().filter(i -> i % 3 == 0 || i % 5 == 0)
+                .reduce(0, (a, b) -> a + b);
 
+        System.out.println(result);
     }
 }
