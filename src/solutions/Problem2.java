3a17ab08d98b6d364ae62c2281442c91961ec363
Mushif Ali Nawaz <mushif07@gmail.com>
2019-02-20 10:39:39 +0500

    Problem 2 Solved

---
+++ src/solutions/Problem2.java
@@ -0,0 +1,25 @@
+package solutions;
+
+/**
+ * @author Mushif Ali Nawaz
+ * Date    February 20, 2019
+ *
+ * Problem 2: Even Fibonacci numbers
+ */
+public class Problem2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int a = 1;
+        int b = 1;
+        int fib = a + b;
+
+        while(fib < 4000000) { // calculate only the 3rd value (i.e. even) in the sequence
+            sum += fib;
+            a = b + fib;
+            b = fib + a;
+            fib = a + b;
+        }
+
+        System.out.println(sum);
+    }
+}
