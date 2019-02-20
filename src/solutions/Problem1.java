65f18b45d9f6b6cd55331733f03b3c6fb617f11a
Mushif Ali Nawaz <mushif07@gmail.com>
2019-02-20 08:49:11 +0500

    Project Setup

---
+++ src/solutions/Problem1.java
@@ -0,0 +1,13 @@
+package solutions;
+
+/**
+ * @author Mushif Ali Nawaz
+ * Date    February 20, 2019
+ *
+ * Problem 1: Multiples of 3 and 5
+ */
+public class Problem1 {
+    public static void main(String[] args) {
+
+    }
+}
