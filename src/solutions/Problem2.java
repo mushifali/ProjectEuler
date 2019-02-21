83c5e7566ce3dada7dc52bdb4595db75e4a5ff70
Mushif Ali Nawaz <mushif07@gmail.com>
2019-02-21 09:11:31 +0500

    Code Refactored

--- src/solutions/Problem2.java
+++ src/solutions/Problem2.java
@@ -3,7 +3,7 @@ package solutions;
 /**
  * @author Mushif Ali Nawaz
  * Date    February 20, 2019
- *
+ * <p>
  * Problem 2: Even Fibonacci numbers
  */
 public class Problem2 {
@@ -13,7 +13,7 @@ public class Problem2 {
         int b = 1;
         int fib = a + b;
 
-        while(fib < 4000000) { // calculate only the 3rd value (i.e. even) in the sequence
+        while (fib < 4000000) { // calculate only the 3rd value (i.e. even) in the sequence
             sum += fib;
             a = b + fib;
             b = fib + a;
---
+++ src/solutions/Problem3.java
@@ -0,0 +1,8 @@
+package solutions;
+
+/**
+ * @author Mushif Ali Nawaz
+ * Date    February 20, 2019
+ */
+public class Problem3 {
+}
