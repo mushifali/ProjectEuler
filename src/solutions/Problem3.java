a35d9b5ca5250021cdb7bdb5e40d738c1ecdf2df
Mushif Ali Nawaz <mushif07@gmail.com>
2019-02-21 09:11:53 +0500

    Problem 3 solved

--- src/solutions/Problem3.java
+++ src/solutions/Problem3.java
@@ -3,6 +3,30 @@ package solutions;
 /**
  * @author Mushif Ali Nawaz
  * Date    February 20, 2019
+ * <p>
+ * Problem 3: Largest prime factor
  */
 public class Problem3 {
+    public static void main(String[] args) {
+        long n = 600851475143L;
+        long primeFactor = 2;
+        long largestPrimeFactor = 0;
+
+        while (n > 1) {
+            while (n % primeFactor == 0) {
+                n /= primeFactor;
+                largestPrimeFactor = primeFactor;
+            }
+
+            primeFactor += primeFactor == 2 ? 1 : 2;
+
+            if (primeFactor * primeFactor > n) {
+                if (n > 1)
+                    largestPrimeFactor = n;
+                break;
+            }
+        }
+
+        System.out.println(largestPrimeFactor);
+    }
 }
